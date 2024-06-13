6848249430fc3b72779d018126c2a9f90bfb6475
stayboogy <161930050+stayboogy@users.noreply.github.com>
2024-06-13 00:17:38 -0500

    v3.0 release
    better notation
    
    squashed unhandled exceptions
    
    add icon to csproj
    
    delete destination file in windows if decryption fails from incorrect password/salt

--- README.md
+++ README.md
@@ -4,14 +4,32 @@
 ## C# Cross Platform Console Application 
 
 
-## Easily Encrypt/Decrypt Files in Windows / Linux / MacOS Terminal
+
+## Supports Windows [x86, amd64], Linux [amd64, arm64], MacOS [amd64, arm64]
 
 
-### Supports Windows [x86, amd64], Linux [amd64, arm64], MacOS [amd64, arm64]
 
+## Easily Encrypt / Decrypt Files in Windows / Linux / MacOS Terminal
+
+- The way the program works is it takes a mode argument, a source file and a destination file, and then a user supplied Password and Salt to derive a key, and encrypt said source file to the destination file.
+
+- The program makes an SHA512 hash of the user supplied Password and Salt values >> then converts the password hash bytes to a hex string but leaves the salt in a byte array format >> then using the converted password hex string coupled with the salt hash byte array uses the Rfc2898DeriveBytes.Pbkdf2 method to create an Encryption Key in a byte array of 32 bytes
+
+- Due to Special Functions active in Linux / MacOS Terminals, Special Key Characters should not be used in Password or Salt. Special Key Characters work fine in Windows for encryption and decryption, but if used in Windows you will not be able to decrypt the file in Linux / MacOS.
+
+- DeCryption happens in reverse using a correct Password and Salt.
+
+- Very Secure Method if I do say so myself.
 
 ### Usage:
 
+- MacOS / Linux - make executable first
+```sh
+chmod +x ./ezcrypt
+```
+
+
+- Running without arguments - ./ezcrypt [MacOS / Linux] | ezcrypt [Windows] - gives the following usage help
 ```sh
 Encrypt or Decrypt a file.
 
@@ -34,20 +52,26 @@ encrypt:       ezcrypt -e inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38
 decrypt:       ezcrypt -d inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38Y6uwq90bn
 ```
 
+
 ### Special Note:
 
 ```sh
-Because of the way Terminal works in Linux/MacOS, certain special characters cause issues in Password & Salt.
+Because of the way Terminal works in Linux/MacOS,
+certain special characters cause issues in Password & Salt.
 
-In Windows, you can use special characters in any location in Password & Salt with No Issues.
+In Windows, you can use special characters
+in any location in Password & Salt with No Issues.
 
-For True Cross Platform Compatibility Do Not Use Special Characters in Password & Salt.
+For True Cross Platform Compatibility,
+Do Not Use Special Characters in Password & Salt.
 
-If only using in Windows, please use special characters for Password & Salt
+If only using EZCrypt in Windows,
+with no plan of decrypting the file in Linux / MacOS,
+use special characters within Password & Salt
 ```
 
-### Usage Examples:
 
+### Usage Examples:
 
 #### Encrypt File:
 
@@ -76,10 +100,34 @@ ezcrypt -d Path:\To\Input\File Path:\To\Output\File password salt
 ```
 
 
-### Build it Yourself in MacOS for All Platforms:
+## Build it Yourself on All Platforms:
+
+MacOS:
 
 ```sh
 brew install dotnet
 cd ezcrypt
 ./build.sh
+```
+
+Linux:
+
+install the dotnet sdk and make sure it is in your $PATH
+```sh
+cd ezcrypt
+./build.sh
+```
+
+Windows:
+
+make sure the dot net sdk is in your path
+```sh
+open ezcrypt dir in cmd prompt
+
+dotnet publish --self-contained -r osx-arm64 -c release -o release/v2.3/MacOS/arm64 -p:PublishSingleFile=true
+dotnet publish --self-contained -r osx-amd64 -c release -o release/v2.3/MacOS/amd64 -p:PublishSingleFile=true
+dotnet publish --self-contained -r win-amd64 -c release -o release/v2.3/Windows/amd64 -p:PublishSingleFile=true
+dotnet publish --self-contained -r win-x86 -c release -o release/v2.3/Windows/i386 -p:PublishSingleFile=true
+dotnet publish --self-contained -r linux-amd64 -c release -o release/v2.3/Linux/amd64 -p:PublishSingleFile=true
+dotnet publish --self-contained -r linux-arm64 -c release -o release/v2.3/Linux/arm64 -p:PublishSingleFile=true
 ```
--- ezcrypt/Program.cs
+++ ezcrypt/Program.cs
@@ -1,28 +1,42 @@
-﻿using System;
+﻿// original source for this work comes from here: 
+// https://github.com/JohnRush/File-Encryption-Tutorial/tree/master/source/Tutorial03
+//
+//
+// I have extensively modified it to make it more secure
+//
+// I have made it Cross Platform Publishable as well - Windows, Linux, MacOS Compatible
+//
+// I have also added error handling to squash unhandled exceptions
+//
+// I have also added lots of clear notation for anyone who wants to learn from this code
+//
+//
+
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
 
-// original source for this work comes from here: https://github.com/JohnRush/File-Encryption-Tutorial/tree/master/source/Tutorial03
-
 namespace EZCrypt   
 {
     class cryptwork
     {
         static void Main(string[] args)
-        {   			
+        { 	  			
+          // determine OS Platform
+          // if running on Windows
           OperatingSystem os = Environment.OSVersion;
           PlatformID pid = os.Platform;
           if (pid is PlatformID.Win32NT)
-		  {
-			  // because this can be used for large files and I use it for large disk images mainly
-              // we run this process with "high priority" by default
-              // very first thing console app does
-              System.Diagnostics.Process ezcrypt = System.Diagnostics.Process.GetCurrentProcess();
-              ezcrypt.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
-           }
+          {
+            // because this can be used for large files and I use it for large disk images mainly
+          	// we run this process with "high priority" by default
+            // very first thing console app does
+            System.Diagnostics.Process ezcrypt = System.Diagnostics.Process.GetCurrentProcess();
+            ezcrypt.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+          }
 		   
             // 1 arg for mode -encrypt or -decrypt
             // 2 arg for input file
@@ -33,30 +47,34 @@ namespace EZCrypt
             // if arguments don't equal 5
             if (args.Length != 5)
             {
-                // we console print the usage help
-                Console.WriteLine("Encrypt or Decrypt a file.");
-                Console.WriteLine("");
-                Console.WriteLine("\nezcrypt [-e] [-d] source destination [password] [salt]\n");
-                Console.WriteLine("");
-                Console.WriteLine("{0,-15}Sets the mode to ENCRYPT.", "-e");
-                Console.WriteLine("{0,-15}Sets the mode to DECRYPT.", "-d");
-                Console.WriteLine("{0,-15}Specifies the input file path.", "source");
-                Console.WriteLine("{0,-15}Specifies the destination file path.", "destination");
-                // original source had password optional - this is not advised for any encryption
-                Console.WriteLine("{0,-15}Specifies the Encryption Password.", "password");
-                // original source had a static defined salt - this is not advised for any encryption
-                Console.WriteLine("{0,-15}Specifies the Encryption Salt.", "salt");
-                Console.WriteLine("");
-                Console.WriteLine("Examples:");
-                Console.WriteLine("");
-                Console.WriteLine("Password & Salt:  Use Numbers and Letters ONLY for Platform Cross Compatibility");
-                Console.WriteLine("");
-                // encrypt example
-                Console.WriteLine("{0,-15}ezcrypt -e inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38Y6uwq90bn", "encrypt:");
-                // decrypt example
-                Console.WriteLine("{0,-15}ezcrypt -d inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38Y6uwq90bn", "decrypt:");
-                Console.WriteLine("");
-                return;
+          	  // we console print the usage help
+              Console.WriteLine("Encrypt or Decrypt a file.");
+              Console.WriteLine("");
+              Console.WriteLine("\nezcrypt [-e] [-d] source destination [password] [salt]\n");
+              Console.WriteLine("");
+              Console.WriteLine("{0,-15}Sets the mode to ENCRYPT.", "-e");
+              Console.WriteLine("{0,-15}Sets the mode to DECRYPT.", "-d");
+              Console.WriteLine("{0,-15}Specifies the input file path.", "source");
+              Console.WriteLine("{0,-15}Specifies the destination file path.", "destination");
+			  
+              // original source had password optional - this is not advised for any encryption
+              Console.WriteLine("{0,-15}Specifies the Encryption Password.", "password");
+			  
+              // original source had a static defined salt - this is not advised for any encryption
+              Console.WriteLine("{0,-15}Specifies the Encryption Salt.", "salt");
+              Console.WriteLine("");
+              Console.WriteLine("Examples:");
+              Console.WriteLine("");
+              Console.WriteLine("Password & Salt:  Use Numbers and Letters ONLY for Platform Cross Compatibility");
+              Console.WriteLine("");
+			  
+              // encrypt example
+              Console.WriteLine("{0,-15}ezcrypt -e inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38Y6uwq90bn", "encrypt:");
+			  
+              // decrypt example
+              Console.WriteLine("{0,-15}ezcrypt -d inputFilePath outputFilePath Trs89Ely3Ui9031 89073ey38Y6uwq90bn", "decrypt:");
+              Console.WriteLine("");
+              return;
             }
 
             // args 1 mode
@@ -64,9 +82,22 @@ namespace EZCrypt
 
             // args 2 input file path
             var sourceFilename = args[1];
+			// if the input file doesn't exist
+			if (!File.Exists(sourceFilename))
+			{
+		      // let the user know something is wrong with their input file path
+			  Console.WriteLine("\nsource file does not exist");
+			  Console.WriteLine("\nconfirm your entry");
+			  Console.WriteLine("\nuse quotations for paths with spaces");
+			  Console.WriteLine("\n");
+			  return;
+		  	}
 
             // args 3 output file path
             var destinationFilename = args[2];
+			
+			// unused formula for adding .ezcrypt extenstion to filename
+			//var destinationFilename = Path.ChangeExtension(args[2], Path.GetExtension(args[2]) + ".ezcrypt");
 						
             // have to create sha512 instance to be able to use HashAlgorithm method
             HashAlgorithm sha = SHA512.Create();
@@ -89,53 +120,74 @@ namespace EZCrypt
             // if arguments equal 5
             if (args.Length == 5)
             {
-                using (var converter = new Rfc2898DeriveBytes(sauce, salt))
-                {
-                    key = converter.GetBytes(32);
-                }
-             }
+              using (var converter = new Rfc2898DeriveBytes(sauce, salt))
+              {
+                key = converter.GetBytes(32);
+              }
+            }
 
             // if we are encrypting a file
             // args 1
             if (mode == "-e")
             {
-                // Encrypt the source file and write it to the destination file.
-                using (var sourceStream = File.OpenRead(sourceFilename))
-                using (var destinationStream = File.Create(destinationFilename))
-                using (var provider = new AesCryptoServiceProvider())
-                {
-                    provider.Key = key;
-
-                    using (var cryptoTransform = provider.CreateEncryptor())
-                    using (var cryptoStream = new CryptoStream(destinationStream, cryptoTransform, CryptoStreamMode.Write))
-                    {
-                        destinationStream.Write(provider.IV, 0, provider.IV.Length);
-                        sourceStream.CopyTo(cryptoStream);
-                    }
+              // encrypt the source file and write it to the destination file.
+              using (var sourceStream = File.OpenRead(sourceFilename))
+              using (var destinationStream = File.Create(destinationFilename))
+              using (var provider = new AesCryptoServiceProvider())
+              {
+                provider.Key = key;
+                using (var cryptoTransform = provider.CreateEncryptor())
+                using (var cryptoStream = new CryptoStream(destinationStream, cryptoTransform, CryptoStreamMode.Write))
+              	{
+                  destinationStream.Write(provider.IV, 0, provider.IV.Length);
+                  sourceStream.CopyTo(cryptoStream);
+	              Console.WriteLine("\nFile EnCryption Complete! \n");
                 }
+              }
             }
             
             // if we are decrypting a file
             // args 1
             else if (mode == "-d")
             {
-                // Decrypt the source file and write it to the destination file.
-                using (var sourceStream = File.OpenRead(sourceFilename))
-                using (var destinationStream = File.Create(destinationFilename))
-                using (var provider = new AesCryptoServiceProvider())
+              // decrypt the source file and write it to the destination file.
+              using (var sourceStream = File.OpenRead(sourceFilename))
+              using (var destinationStream = File.Create(destinationFilename))
+              using (var provider = new AesCryptoServiceProvider())
+              {
+                var IV = new byte[provider.IV.Length];
+                sourceStream.Read(IV, 0, IV.Length);
+                using (var cryptoTransform = provider.CreateDecryptor(key, IV))
+                using (var cryptoStream = new CryptoStream(sourceStream, cryptoTransform, CryptoStreamMode.Read))
                 {
-                    var IV = new byte[provider.IV.Length];
-                    sourceStream.Read(IV, 0, IV.Length);
-                    using (var cryptoTransform = provider.CreateDecryptor(key, IV))
-                    using (var cryptoStream = new CryptoStream(sourceStream, cryptoTransform, CryptoStreamMode.Read))
-                    {
-                        cryptoStream.CopyTo(destinationStream);
-                    }
+				  try
+				  {
+                    cryptoStream.CopyTo(destinationStream);
+	                Console.WriteLine("\nFile DeCryption Complete! \n");
+				  }
+				  // if the Password and/or Salt is incorrect
+				  catch
+				  {
+					 // close the file copy stream so file can be deleted if created
+					 // this is mainly for Windows
+					 destinationStream.Close();
+					 // let the user know their Password and/or Salt is incorrect
+ 					 Console.WriteLine("\nPassword + Salt combination incorrect");
+					 Console.WriteLine("\nconfirm your Password");
+					 Console.WriteLine("\nconfirm your Salt");
+	   			     Console.WriteLine("\n");
+					 File.Delete(destinationFilename);
+					 // delete the (possibly) created file that is not decrypted due to incorrect password and/or salt
+				  }
                 }
+              }
             }
+			
+			// if something other than -e and/or -d is used for args 1
             else
             {
-                Console.WriteLine("Must select the ecryption/decryption mode using -e or -d.");
+		      // let the user know to user either -e and/or -d only
+              Console.WriteLine("select the ecryption/decryption mode using -e or -d.");
             }
         }
     }
