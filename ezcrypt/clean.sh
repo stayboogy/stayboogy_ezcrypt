c261f4cc72e257daf3640aee4c9aae88659b287d
stayboogy <161930050+stayboogy@users.noreply.github.com>
2024-06-13 22:40:59 -0500

    initial commit for diffs branch

--- ezcrypt/clean.sh
+++
@@ -1,6 +0,0 @@
-rm release/v4.0/Linux/arm64/ezcrypt.pdb
-rm release/v4.0/Linux/amd64/ezcrypt.pdb
-rm release/v4.0/MacOS/arm64/ezcrypt.pdb
-rm release/v4.0/MacOS/amd64/ezcrypt.pdb
-rm release/v4.0/Windows/amd64/ezcrypt.pdb
-rm release/v4.0/Windows/i386/ezcrypt.pdb
--- ezcrypt/test.sh
+++
@@ -1,3 +0,0 @@
-cp test/test.txt release/v4.0/MacOS/arm64/
-cp test/eztest.sh release/v4.0/MacOS/arm64/
-release/v4.0/MacOS/arm64/eztest.sh
